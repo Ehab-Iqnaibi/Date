05dc2e6843b75f136b81cebff47e4dc3e7ccd87e
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-10-30 22:44:07 +0200

    ehab date

--- date.py
+++ date.py
@@ -1,5 +1,4 @@
 class Date:
-
     def __init__(self, day, month, year):
         self.MonthD= [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
         self.MonthN = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
@@ -9,9 +8,7 @@ class Date:
         self.year = int(year)
         if self.isValidDate():
             print("Input date is Valid!")
-
     def isValidDate(self):
-        #print(self.MonthD[self.month - 1])
         if 1 <= self.month <= 12 and 1 <= self.day <= self.MonthD[self.month -1]:
             isValidDate = True
         else:
@@ -26,10 +23,48 @@ class Date:
                   order=order+self.MonthD[i-1]
               elif i == self.month:
                   order=order+self.day
+              i+=1
         return order
-
     def __str__(self):
         if self.isValidDate():
            return str(self.day) + " / " + str(self.month) + " / " + str(self.year)
         else:
            return ("Input date is Invalid")
+    def __add__(self,other):
+        if self.isValidDate():
+             N=self.MonthD[self.month -1]-self.day
+             if N >= other:
+                 self.day+=other
+             elif N< other:
+                 if self.month < 12:
+                     self.month+=1
+                 elif self.month == 12:
+                    self.year+=1
+                    self.month=1
+                 self.day=other -N
+             #print(self)
+             #return  (self.day,self.month,self.year)
+             return(self)
+    def __sub__(self,other):
+        if self.isValidDate() and other.isValidDate():
+            n1=self.year*365 + self.day
+            for k in range(0,self.month-1):
+                n1+=self.MonthD[k]
+            n2 = other.year * 365 + other.day
+            for k in range(0, other.month - 1):
+                n2 += other.MonthD[k]
+            n=abs(n1-n2)
+            return n
+
+    def __lt__(self, other):
+        return self.order() < other.order()
+    def __gt__(self, other):
+        return self.order() > other.order()
+    def __le__(self, other):
+        return self.order() <= other.order()
+    def __ge__(self, other):
+        return self.order() >= other.order()
+    def __eq__(self, other):
+        return self.order() == other.order()
+    def __ne__(self, other):
+        return self.order() != other.order()
--- main.py
+++ main.py
@@ -1,26 +1,77 @@
 from date import Date
-'''
-    def get_date(self):
-        inputDate = input("Enter the date in format 'dd/mm/yy' : ")
-        #day,month,year = inputDate.split('/')
-        #date1= Date(int(day), int(month),  int(year))
-'''
 
 
-    #def put_date(self):
-        #print ("Input date: " +inputDate)
+while user != 'esc':
+        day,month,year = input("Enter the date in format 'dd,mm,yyyy' : ").split(' , ')
+        d1= Date(int(day), int(month),  int(year))
+        print(d1)
+        d2=input("Enter the 2nd date in format 'dd,mm,yyyy' : ").split(' , ')
+        d1 = Date(int(day), int(month), int(year))
+        print(d2)
+
+        enter=1
+        while enter:
+            user1= int(input('''what test you want to perform ??
+                1. add a number to date ?
+                2. Order of date?
+                3. subtract the first and the second dates ? 
+                4. First Date order > second date order?
+                5. First Date order < second date order?
+                6. First Date order >= second date order?
+                7. First Date order <= second date order?
+                8. First Date order = second date order?
+                9. First Date order != second date order? '''))
+            if user1 == 1:
+                user2 = int(input('1. d1 OR 2. d2: ' )
+                if user2 == 1:
+                  print(d1 + int(input('Enter a number to add to first Date= ')))
+                if user2 == 2:
+                  print(d2 + int(input('Enter a number to add to first Date= ')))
+            elif user1 == 2:
+                 user2 = int(input('1. d1 OR 2. d2: ')
+                 if user2 == 1:
+                   print(d1.order())
+                 if user2 == 2:
+                   print(d2.order())
+            elif user1 == 3:
+                 print(d1 - d2)
+            elif user1 == 4:
+                 print(d1 > d2)
+            elif user1 == 5:
+                print(d1 < d2)
+            elif user1 == 6:
+                print(d1 >= d2)
+            elif user1 == 7:
+                print(d1 <= d2)
+            elif user1 == 8:
+                print(d1 == d2)
+            elif user1 == 9:
+                print(d1 != d2)
+
+
 
 
 d1 = Date(23, 12, 2020)
 print(d1)
-print(d1.order())
-'''
+my_order=d1.order()
+print(my_order)
+
 d2 = Date(23, 13, 2020)
 print(d2)
 print(d2.order())
-d3= Date(40, 6, 2020)
+
+d3 = Date(13, 7, 2019)
 print(d3)
 print(d3.order())
-#date1.get_date()
-#date1.put_date()
-'''
+
+d4=d1-d3
+print('number of days between two dates: ',str(d4))
+
+update=d1+22
+dd, mm,yyyy =update
+print(update,type(update))
+update = Date(int(dd), int(mm), int(yyyy))
+my_order=update.order()
+print(my_order)
+
+
