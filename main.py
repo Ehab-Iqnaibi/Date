25a980dd8e3d4b516dae59dfe962812b7159ead1
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-10-31 09:45:01 +0200

    ehab f

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 from date import Date
 
-
-while user != 'esc':
-        day,month,year = input("Enter the date in format 'dd,mm,yyyy' : ").split(' , ')
-        d1= Date(int(day), int(month),  int(year))
+while True:
+        day1,month1,year1 = input("Enter the date in format 'dd,mm,yyyy' : ").split(',')
+        d1= Date(int(day1), int(month1),  int(year1))
         print(d1)
-        d2=input("Enter the 2nd date in format 'dd,mm,yyyy' : ").split(' , ')
-        d1 = Date(int(day), int(month), int(year))
+        day2,month2,year2=input("Enter the 2nd date in format 'dd,mm,yyyy' : ").split(',')
+        d2 = Date(int(day2), int(month2), int(year2))
         print(d2)
 
-        enter=1
-        while enter:
-            user1= int(input('''what test you want to perform ??
-                1. add a number to date ?
-                2. Order of date?
-                3. subtract the first and the second dates ? 
-                4. First Date order > second date order?
-                5. First Date order < second date order?
-                6. First Date order >= second date order?
-                7. First Date order <= second date order?
-                8. First Date order = second date order?
-                9. First Date order != second date order? '''))
+        while True:
+            user1= int(input('''
+                1. Add a number to date :
+                2. Order day in the year of date:
+                3. subtract the first and the second dates : 
+                4. First date order > Second date order?
+                5. First date order < Second date order?
+                6. First date order >= Second date order?
+                7. First date order <= Second date order?
+                8. First date order = Second date order?
+                9. First date order != Second date order?
+                10. Do you want to show the month in words?
+                Choos the number of the operation to perform: '''))
             if user1 == 1:
-                user2 = int(input('1. d1 OR 2. d2: ' )
+                user2 = int(input('1. d1 OR 2. d2: ' ))
                 if user2 == 1:
-                  print(d1 + int(input('Enter a number to add to first Date= ')))
-                if user2 == 2:
-                  print(d2 + int(input('Enter a number to add to first Date= ')))
+                   en=int(input('Enter a number to add to first Date= '))
+                   print('Next date after '+str(en)+' days: '+str(d1 + en))
+                elif user2 == 2:
+                   en=int(input('Enter a number to add to second Date= '))
+                   print('Next date after '+str(en)+' days: '+ str( d2 + en))
             elif user1 == 2:
-                 user2 = int(input('1. d1 OR 2. d2: ')
-                 if user2 == 1:
-                   print(d1.order())
-                 if user2 == 2:
-                   print(d2.order())
+                user2 = int(input('1. d1 OR 2. d2: '))
+                if user2 == 1:
+                   print('Order of the date is: '+str(d1.order()))
+                elif user2 == 2:
+                   print('Order of the date is: '+str(d2.order()))
             elif user1 == 3:
-                 print(d1 - d2)
+                 print('The difference (number of days) between two dates: '+str(d1 - d2))
             elif user1 == 4:
                  print(d1 > d2)
             elif user1 == 5:
@@ -47,31 +49,22 @@ while user != 'esc':
                 print(d1 == d2)
             elif user1 == 9:
                 print(d1 != d2)
+            elif user1 == 10:
+                user2 = int(input('1.d1 OR 2.d2: '))
+                if user2 == 1:
+                    print(d1.MonthN[int(month1) -1])
+                elif user2 == 2:
+                    print(d2.MonthN[int(month2) -1])
 
+            user_input = input('Do you want to choose another operation (yes/no): ')
+            if user_input.lower() == 'yes':
+                continue
+            elif user_input.lower() == 'no':
+                break
+        user_input = input('Do you want to choose another dates (yes/no): ')
+        if user_input.lower() == 'yes':
+            continue
+        elif user_input.lower() == 'no':
+            break
 
-
-
-d1 = Date(23, 12, 2020)
-print(d1)
-my_order=d1.order()
-print(my_order)
-
-d2 = Date(23, 13, 2020)
-print(d2)
-print(d2.order())
-
-d3 = Date(13, 7, 2019)
-print(d3)
-print(d3.order())
-
-d4=d1-d3
-print('number of days between two dates: ',str(d4))
-
-update=d1+22
-dd, mm,yyyy =update
-print(update,type(update))
-update = Date(int(dd), int(mm), int(yyyy))
-my_order=update.order()
-print(my_order)
-
-
+print('Thank you :)')
